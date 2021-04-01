e76e1ecec02fb8ca5f690a9b7d6e9785dd65d9c8
Thorsten Scherf <tscherf@redhat.com>
2021-04-01 10:41:29 +0200

    fix some issues with CA_DIR

--- create-openssl-ca.sh
+++ create-openssl-ca.sh
@@ -4,17 +4,17 @@ CA_DIR=/etc/pki/CA/
 OPENSSL_CONF=/etc/pki/tls/openssl.cnf
 
 for name in certs private crl newcerts newcerts/private csr; do
-    [[ ! $name ]] || mkdir $name
+    [[ ! $CA_DIR/$name ]] || mkdir $name
 done
 
-chmod 700 private
-touch index.txt
-echo 1000 > serial
-echo 1000 > crlnumber
+chmod 700 $CA_DIR/newcerts/private
+touch $CA_DIR/index.txt
+echo 1000 > $CA_DIR/serial
+echo 1000 > $CA_DIR/crlnumber
 
 
 # Create the key
-openssl genrsa -aes256 -out private/ca.key.pem 4096
+openssl genrsa -aes256 -out $CA_DIR/private/ca.key.pem 4096
 
 chmod 400 private/ca.key.pem
 
@@ -24,6 +24,6 @@ openssl req -config $OPENSSL_CONF \
     -new -x509 -days 7300 -sha256 -extensions v3_ca \
     -out $CA_DIR/certs/ca.cert.pem
 
-chmod 444 certs/ca.cert.pem
+chmod 444 $CA_DIR/certs/ca.cert.pem
 
 
